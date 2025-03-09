fc9d1b6ecf575d451468b08c827d5f485d9cd95f
Reazul Islam Fahim <127842167+Reazul-Islam-Fahim@users.noreply.github.com>
2025-03-09 12:27:33 +0600

    Update customButton.dart

--- lib/widgets/customButton.dart
+++ lib/widgets/customButton.dart
@@ -2,20 +2,30 @@ import 'package:flutter/material.dart';
 import 'package:test_app/const/AppColors.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
-Widget customButton (String buttonText,onPressed){
+Widget customButton(
+    String buttonText,
+    VoidCallback onPressed, {
+      bool isLoading = false,
+    }) {
   return SizedBox(
     width: 1.sw,
     height: 56.h,
     child: ElevatedButton(
-      onPressed: onPressed,
-      child: Text(
+      onPressed: isLoading ? null : onPressed, // Disable if loading
+      child: isLoading
+          ? CircularProgressIndicator(
+        valueColor: AlwaysStoppedAnimation<Color>(Colors.white),
+      )
+          : Text(
         buttonText,
-        style: TextStyle(
-            color: Colors.white, fontSize: 18.sp),
+        style: TextStyle(color: Colors.white, fontSize: 18.sp),
       ),
       style: ElevatedButton.styleFrom(
         backgroundColor: AppColors.blue,
         elevation: 3,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(30.r),
+        ),
       ),
     ),
   );
