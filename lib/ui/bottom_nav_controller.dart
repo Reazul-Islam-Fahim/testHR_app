45428b26c485c21a8156691b58d60182dbeed511
Reazul Islam Fahim <127842167+Reazul-Islam-Fahim@users.noreply.github.com>
2025-03-13 12:59:56 +0600

    Update bottom_nav_controller.dart

--- lib/ui/bottom_nav_controller.dart
+++ lib/ui/bottom_nav_controller.dart
@@ -23,6 +23,10 @@ class _BottomNavControllerState extends State<BottomNavController> {
   bool hasPendingLeave = false;
   bool hasPendingExpenses = false;
 
+  String? _designation;
+  // bool _isLoading = false;
+  // bool _hasError = false;
+
   final _pages = [
     Leave(),
     Expense(),
@@ -63,6 +67,35 @@ class _BottomNavControllerState extends State<BottomNavController> {
     }
   }
 
+  Future<void> _fetchUserData() async {
+    // setState(() {
+    //   _isLoading = true;
+    //   _hasError = false; // Reset error state
+    // });
+
+    try {
+      // Replace with your API URL to fetch user data
+      final response =
+          await http.get(Uri.parse('http://192.168.3.228:7000/get-user-data'));
+
+      if (response.statusCode == 200) {
+        // Parse the JSON response
+        final data = json.decode(response.body);
+
+        setState(() {
+          _designation = data['designation'];
+        });
+      } else {
+        throw Exception('Failed to load data');
+      }
+    } catch (e) {
+      // setState(() {
+      //   _hasError = true;
+      // });
+      print("Error fetching data: $e");
+    }
+  }
+
   @override
   void initState() {
     super.initState();
@@ -71,6 +104,7 @@ class _BottomNavControllerState extends State<BottomNavController> {
     // Call the functions to check for pending requests
     checkPendingLeaves();
     checkPendingExpenses();
+    _fetchUserData();
   }
 
   @override
@@ -102,30 +136,37 @@ class _BottomNavControllerState extends State<BottomNavController> {
               icon: Icon(Icons.notifications),
               label: "Notification",
             ),
-            if (hasPendingLeave ||
-                hasPendingExpenses)
-              BottomNavigationBarItem(
-                icon: Stack(
-                  children: [
-                    Icon(Icons.more_horiz),
-                    Positioned(
-                      right: 0,
-                      child: Container(
-                        padding: EdgeInsets.all(1),
-                        decoration: BoxDecoration(
-                          color: Colors.red,
-                          borderRadius: BorderRadius.circular(6),
-                        ),
-                        constraints: BoxConstraints(
-                          minWidth: 12,
-                          minHeight: 12,
+            if (_designation?.toLowerCase() == 'manager' ||
+                _designation?.toLowerCase() == 'admin')
+              ...[
+              if (hasPendingLeave || hasPendingExpenses)
+                BottomNavigationBarItem(
+                  icon: Stack(
+                    children: [
+                      Icon(Icons.more_horiz),
+                      Positioned(
+                        right: 0,
+                        child: Container(
+                          padding: EdgeInsets.all(1),
+                          decoration: BoxDecoration(
+                            color: Colors.red,
+                            borderRadius: BorderRadius.circular(6),
+                          ),
+                          constraints: BoxConstraints(
+                            minWidth: 12,
+                            minHeight: 12,
+                          ),
                         ),
                       ),
-                    ),
-                  ],
-                ),
-                label: "More",
-              )
+                    ],
+                  ),
+                  label: "More",
+                )
+              else
+                BottomNavigationBarItem(
+                  icon: Icon(Icons.more_horiz),
+                  label: "More",
+                ),]
             else
               BottomNavigationBarItem(
                 icon: Icon(Icons.more_horiz),
